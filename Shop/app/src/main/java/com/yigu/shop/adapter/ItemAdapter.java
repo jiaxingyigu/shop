0b56dec0849264e9f5c319e41f679afa8d86228f
zhuyaojie <zyjqjnh@126.com>
2016-09-08 15:17:37 +0800

    添加默认图片

--- Shop/app/src/main/java/com/yigu/shop/adapter/ItemAdapter.java
+++ Shop/app/src/main/java/com/yigu/shop/adapter/ItemAdapter.java
@@ -5,42 +5,69 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.RelativeLayout;
 
 import com.yigu.shop.R;
 import com.yigu.shop.commom.result.MapiItemResult;
+import com.yigu.shop.shopinterface.RecyOnItemClickListener;
 
 import java.util.List;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
  * Created by brain on 2016/9/1.
  */
-public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     private LayoutInflater inflater;
     List<MapiItemResult> mList;
+    private int selPosition;
+    private RecyOnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(RecyOnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
     public ItemAdapter(Context context, List<MapiItemResult> list) {
         inflater = LayoutInflater.from(context);
         mList = list;
     }
 
+    public void setSelPosition(int position){
+        selPosition = position;
+    }
+
     @Override
     public int getItemCount() {
-        return mList==null?0:mList.size();
+        return mList == null ? 0 : mList.size();
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(inflater.inflate(R.layout.item_product,parent,false));
+        return new ViewHolder(inflater.inflate(R.layout.item_product, parent, false));
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.itemRoot.setTag(position);
+        holder.itemRoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selPosition != (Integer) view.getTag()) {
+                    selPosition = (Integer) view.getTag();
 
+                    if(null!=onItemClickListener)
+                        onItemClickListener.onItemClick(view,(Integer)view.getTag());
+                }
+                notifyDataSetChanged();
+            }
+        });
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.item_root)
+        RelativeLayout itemRoot;
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
